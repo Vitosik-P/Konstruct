58b855df5ba454ee47605db96cb0b9c34e2f5c29
Vitos <fruzi808@gmail.com>
2021-02-06 13:40:31 +0200

    Create project

---
+++ script.js
@@ -0,0 +1,9 @@
+let navBtn = document.querySelector('.nav__btn');
+let nav = document.querySelector('.nav__item_list');
+let navBg = document.querySelector('.nav__bg');
+
+navBtn.addEventListener('click', function(){
+	navBtn.classList.toggle('nav__btn-active');
+	nav.classList.toggle('nav__item_list-active');
+    navBg.classList.toggle('nav__bg-active')
+})
