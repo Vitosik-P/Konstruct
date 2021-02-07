d3f21ea5829ffda4f2856e044028b5e291eb2e4d
Vitos <fruzi808@gmail.com>
2021-02-07 16:58:17 +0200

    Add 'X' for burger menu

--- script.js
+++ script.js
@@ -5,5 +5,11 @@ let navBg = document.querySelector('.nav__bg');
 navBtn.addEventListener('click', function(){
 	navBtn.classList.toggle('nav__btn-active');
 	nav.classList.toggle('nav__item_list-active');
-    navBg.classList.toggle('nav__bg-active')
+    navBg.classList.toggle('nav__bg-active');
+});
+
+nav.addEventListener('click', function(){
+	navBtn.classList.toggle('nav__btn-active');
+	nav.classList.toggle('nav__item_list-active');
+    navBg.classList.toggle('nav__bg-active');
 })
