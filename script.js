599b6b5962f74a12cb496c2940ea142db501c09a
Vitos <fruzi808@gmail.com>
2021-02-09 00:37:07 +0200

    Update menu and footer

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let navBtn = document.querySelector('.nav__btn');
-let nav = document.querySelector('.nav__item_list');
-let navBg = document.querySelector('.nav__bg');
+let navBtn = document.querySelector('.nav__btn')
+let nav = document.querySelector('.nav__item_list')
+let navBg = document.querySelector('.nav__bg')
 
-navBtn.addEventListener('click', function(){
-	navBtn.classList.toggle('nav__btn-active');
-	nav.classList.toggle('nav__item_list-active');
-    navBg.classList.toggle('nav__bg-active');
-});
+navBtn.addEventListener('click', () => {
+  navBtn.classList.toggle('nav__btn-active')
+  nav.classList.toggle('nav__item_list-active')
+  navBg.classList.toggle('nav__bg-active')
+})
 
-nav.addEventListener('click', function(){
-	navBtn.classList.toggle('nav__btn-active');
-	nav.classList.toggle('nav__item_list-active');
-    navBg.classList.toggle('nav__bg-active');
+nav.addEventListener('click', () => {
+  navBtn.classList.toggle('nav__btn-active')
+  nav.classList.toggle('nav__item_list-active')
+  navBg.classList.toggle('nav__bg-active')
 })
